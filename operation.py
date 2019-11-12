fbd07eb163019e2f6b00ca60337ef994dcd1320e
Kseniya <kseniya.shevchuk@phystech.edu>
2019-11-12 11:57:07 +0300

    Define class Operator, add class Plus.
    Method result return value of Number.

--- operation.py
+++ operation.py
@@ -1,4 +1,27 @@
 import number
 
 class Operation:
-	pass
+	def __init__(self,symbol_of_operation,operand1,operand2):
+		self.type = symbol_of_operation
+		self.entries = [operand1,operand2]
+	
+	"""
+	Return value of Number, type int or float.
+	"""
+	def result(self):
+		pass
+	
+	def get_op(self):
+		return str(self.entries[0])+self.type+str(self.entries[1])
+
+class Plus(Operation):
+	def __init__(self,operand1,operand2):
+		super().__init__('+',operand1,operand2)
+	
+	def result(self):
+		return self.entries[0]+self.entries[1]
+	
+	def print_result(self):
+		return self.get_op() + '=' + str(self.result())
+
+
