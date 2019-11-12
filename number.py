094ac833957ca08dac20390ee6a42919c4656214
Kseniya <kseniya.shevchuk@phystech.edu>
2019-11-12 11:43:56 +0300

    Develop class Number.
    Processs init and getNumber.

--- number.py
+++ number.py
@@ -1,2 +1,11 @@
 class Number:
-	pass
+	def __init__(self, value, sign):
+		self.value = value
+		self.sign = sign
+		self.type = type(self.value)
+		if self.type not in (int, float):
+			self.type = 'other'
+			raise TypeError('Not a number!')
+	
+	def getNumber(self):
+		return self.value
